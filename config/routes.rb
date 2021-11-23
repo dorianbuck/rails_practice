66a2fbd1a221c578539eb2bae0e01860062e6019
Dorian Buck <dorianbuck@gmail.com>
2021-11-23 19:15:29 +0100

    adds passing tests for create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   namespace :api do
     resources :movies, only: %i[index create]
   end
-  # get '/api/movies', controller: :movies, action: :index
 end
--- spec/request/api/movies/create_spec.rb
+++ spec/request/api/movies/create_spec.rb
@@ -1,8 +1,13 @@
 RSpec.describe 'POST /api/movies', type: :request do
-subject { response }
+  subject { response }
   describe 'when movie is created' do
     before do
-      post '/api/movies', params: { movie: { title: 'title', genre: 'horror', year: '2022-01-01', description: 'Lorem' } }
+      post '/api/movies',
+           params: { movie: { title: 'title',
+                              genre: 'horror',
+                              year: '2022-01-01',
+                              description: 'Lorem',
+                              image: 'placeholder' } }
     end
     it { is_expected.to have_http_status 201 }
 
@@ -18,7 +23,7 @@ subject { response }
     it { is_expected.to have_http_status 422 }
 
     it 'returns an error message' do
-      expect(response.body).to match(/Validation failed: Title can't be blank, Genre can't be blank, Year can't be blank, Description can't be blank/)
+      expect(response_json['error']).to eq("Title can't be blank, Genre can't be blank, Year can't be blank, Description can't be blank, Image can't be blank")
     end
   end
 end
